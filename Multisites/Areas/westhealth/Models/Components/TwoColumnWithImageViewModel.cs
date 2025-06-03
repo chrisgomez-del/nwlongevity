d73301eb595840edb6e2b7338e859d1ad1adbdfc
Jerod Kruszka <jkruszka@laughlin.com>
2025-06-03 10:29:40 -0500

    Styling of two column with image component

--- Multisites/Areas/westhealth/Models/Components/TwoColumnWithImageViewModel.cs
+++ Multisites/Areas/westhealth/Models/Components/TwoColumnWithImageViewModel.cs
@@ -8,5 +8,8 @@ namespace NM_MultiSites.Areas.westhealth.Models.Components
         public Item SourceItem { get; set; }
         public IHtmlString Image { get; set; }
         public string ImageLocation { get; set; }
+        public string ImageLocationCssClass { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageAltText { get; set; }
     }
 }
