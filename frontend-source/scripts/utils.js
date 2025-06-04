589673a6bf90c4ab4441b7f1eb101caee2661348
mjanowski-laughlin <mjanowski@laughlin.com>
2025-06-04 14:34:35 -0500

    update with FE rips

--- frontend-source/scripts/utils.js
+++ frontend-source/scripts/utils.js
@@ -1,3 +1,5 @@
+import gsap from 'gsap';
+
 const bootstrapBreakpoints = {
     xs: 0,
     sm: 576,
@@ -50,4 +52,57 @@ export function matchBreakpoint(queryKey) {
     }
 
     return window.matchMedia(mediaQuery);
+}
+
+export function smoothScrollInternalLinks() {
+    document.querySelectorAll('a[href^="#"]').forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const rawSelector = this.getAttribute("href"); // e.g., "#body" or "#.myclass"
+            if (!rawSelector || rawSelector === "#") return;
+
+            const selector = rawSelector.slice(1); // Remove the '#'
+            let target =
+                document.querySelector(`[data-scrollToTarget='${selector}']`) ||
+                document.querySelector(selector.startsWith('.') || selector.startsWith('#') ? selector : `#${selector}`);
+
+            if (target) {
+                target.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+}
+
+export function animateGradient(selector) {
+    const host = document.querySelector(selector);
+    if (!host) return;
+
+    host.classList.add('gradient-host');
+
+    gsap.timeline({ repeat: -1 })
+        .to(host, {
+            duration: 2.5,
+            '--x': '30%', '--y': '70%',
+            ease: 'power2.inOut'
+        })
+        .to(host, {
+            duration: 2.5,
+            '--x': '100%', '--y': '100%',
+            ease: 'circ.inOut'
+        })
+        .to(host, {
+            duration: 4,
+            '--x': '0%', '--y': '100%',
+            ease: 'sine.inOut'
+        })
+        .to(host, {
+            duration: 2.5,
+            '--x': '50%', '--y': '0%',
+            ease: 'power3.inOut'
+        })
+        .to(host, {
+            duration: 2.5,
+            '--x': '40%', '--y': '50%',
+            ease: 'sine.inOut'
+        });
 }
