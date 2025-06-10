22c27ddbbd70cd6045d71210de2a253942335ccb
Mike Janowski <NM360263@nm.org>
2025-06-10 12:36:00 -0500

    active state for nav

--- Multisites/Areas/westhealth/Models/GenericLinkViewModel.cs
+++ Multisites/Areas/westhealth/Models/GenericLinkViewModel.cs
@@ -16,6 +16,7 @@ namespace NM_MultiSites.Areas.westhealth.Models
     }
     public class NavigationLinkViewModel : GenericLinkViewModel
     {
+        public bool IsActive { get; set; }
         public List<NavigationLinkViewModel> Children { get; set; } = new List<NavigationLinkViewModel>();
     }
 }
