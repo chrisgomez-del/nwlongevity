6877c42cbf8c847e4ff7d5c3980e660c699ffa1d
Jerod Kruszka <jkruszka@laughlin.com>
2025-07-29 11:38:14 -0500

    Add sortorder.

--- Multisites/Areas/westhealth/Models/GenericLinkViewModel.cs
+++ Multisites/Areas/westhealth/Models/GenericLinkViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Sitecore.Pipelines.GetAboutInformation;
 
 namespace NM_MultiSites.Areas.westhealth.Models
 {
@@ -18,5 +19,6 @@ namespace NM_MultiSites.Areas.westhealth.Models
     {
         public bool IsActive { get; set; }
         public List<NavigationLinkViewModel> Children { get; set; } = new List<NavigationLinkViewModel>();
+        public int Sortorder { get; set; }
     }
 }
