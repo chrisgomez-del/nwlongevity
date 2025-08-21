3bb12aee7d6a278545589d74590e83ab99ae200a
Sai <isaiplus@gmail.com>
2025-08-21 10:36:59 -0500

    Base Scaffolding

---
+++ Multisites/Areas/longevity/longevityAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace NM_MultiSites.Areas.longevity
+{
+    public class longevityAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "longevity";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "longevity_default",
+                "longevity/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
